7e09c46f621ae25926cde48612964a0a70873d20
Joshua Duffney <jduffney@microsoft.com>
2023-07-10 13:35:41 -0500

    Mod: remove mapArch func
    Signed-off-by: Joshua Duffney <jduffney@microsoft.com>

--- setup/lib/utils.js
+++ setup/lib/utils.js
@@ -4,16 +4,7 @@ const path = require('path');
 const execSync = require('child_process').execSync;
 const semver = require('semver');
 const core = require('@actions/core');
-
-// arch in [arm, x32, x64...] (https://nodejs.org/api/os.html#os_os_arch)
-// return value in [amd64, 386, arm]
-function mapArch(arch) {
-    const mappings = {
-        x32: '386',
-        x64: 'amd64'
-    };
-    return mappings[arch] || arch;
-}
+const arch = "amd64";
 
 // os in [darwin, linux, win32...] (https://nodejs.org/api/os.html#os_os_platform)
 // return value in [darwin, linux, windows]
@@ -27,7 +18,7 @@ function mapOS(os) {
 
 function getDownloadObject(version) {
     const platform = os.platform();
-    const filename = `notation_${version}_${mapOS(platform)}_${mapArch(os.arch())}`;
+    const filename = `notation_${version}_${mapOS(platform)}_${arch}`;
     const extension = platform === 'win32' ? 'zip' : 'tar.gz';
     const binPath = platform === 'win32' ? 'bin' : path.join(filename, 'bin');
     const url = `https://github.com/notaryproject/notation/releases/download/v${version}/${filename}.${extension}`;
@@ -41,7 +32,7 @@ function getDownloadObject(version) {
 function getDownloadPluginObject(name, version) {
     if (name == "notation-azure-kv") {
         const platform = os.platform();
-        const filename = `${name}_${version}_${mapOS(platform)}_${mapArch(os.arch())}`;
+        const filename = `${name}_${version}_${mapOS(platform)}_${arch}`;
         const extension = platform === 'win32' ? 'zip' : 'tar.gz';
         const url = `https://github.com/Azure/notation-azure-kv/releases/download/v${version}/${filename}.${extension}`;
         const HOME = process.env.HOME;
