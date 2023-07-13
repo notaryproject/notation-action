f659e1793ec263610fec8b6a3a2be918e82e9db1
Patrick Zheng <patrickzheng@microsoft.com>
2023-07-13 13:17:10 +0800

    updated trust store logics
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- README.md
+++ README.md
@@ -1,31 +1,36 @@
 # Notation Action
-Github Action for `notation setup`, `notation sign` and `notation verify`.
+Github Action for `setup notation`, `notation sign` and `notation verify`.
 # Usage
 1. notation-verify:
-    ```sh
+    ```yaml
     - name: verify released artifact
       uses: notaryproject/notation-action/verify@main
       with:
         target_artifact_reference: <target_artifact_reference_in_remote_registry>
-        trust_policy_filepath: <file_path_to_user_defined_trustpolicy.json>
-        trust_store_dir: <dir_to_user_trust_store>
+        trust_policy: <file_path_to_user_defined_trustpolicy.json>
+        trust_store: <dir_to_user_trust_store>
     ```
    For example,
-    ```sh
+    ```yaml
     - name: verify released artifact
       uses: notaryproject/notation-action/verify@main
       with:
         target_artifact_reference: myRegistry.azurecr.io/myRepo@sha256:aaabbb
-        trust_policy_filepath: .github/trustpolicy/trustpolicy.json
-        trust_store_dir: .github/truststore
+        trust_policy: .github/trustpolicy/trustpolicy.json
+        trust_store: .github/truststore
     ```
-    where `.github/truststore` MUST be in following structure:
+    where `.github/truststore` MUST follow the Notation trust store specs (https://github.com/notaryproject/notaryproject/blob/main/specs/trust-store-trust-policy.md#trust-store).
+    
+    For example,
     ```
     .github/truststore
-      |- trust_store1
-          |- certificate1
-          |- certificate2
-      |- trust_store2
-          |- certificate3
-          |- certificate4
+      |- x509
+        |- ca
+          |- <my_trust_store1>
+            |- <my_certificate1>
+            |- <my_certificate2>
+        |- signingAuthority
+          |- <my_trust_store2>
+            |- <my_certificate3>
+            |- <my_certificate4>
     ```
--- verify/index.ts
+++ verify/index.ts
@@ -3,19 +3,24 @@ import { execSync } from 'child_process';
 import * as fs from 'fs';
 import * as path from 'path';
 
+const X509 = "x509";
+
 // verify target artifact with Notation
 async function verify() {
     try {
         const target_artifact_ref = core.getInput('target_artifact_reference');
-        const trust_policy_filepath = core.getInput('trust_policy_filepath');
-        const trust_store_dir = core.getInput('trust_store_dir'); // .github/truststore
+        const trust_policy = core.getInput('trust_policy');
+        const trust_store = core.getInput('trust_store'); // .github/truststore
         let output;
-        execSync(`notation policy import ${trust_policy_filepath}`);
+        // configure Notation trust policy
+        execSync(`notation policy import ${trust_policy}`);
         output = execSync(`notation policy show`, { encoding: 'utf-8' });
         console.log(output);
-        addCerts(trust_store_dir);
+        // configure Notation trust store
+        configTrustStore(trust_store);
         output = execSync(`notation cert ls`, { encoding: 'utf-8' });
         console.log(output);
+        // verify core logic
         if (process.env.NOTATION_EXPERIMENTAL) {
             output = execSync(`notation verify --allow-referrers-api ${target_artifact_ref} -v`, { encoding: 'utf-8' });
         } else {
@@ -31,19 +36,37 @@ async function verify() {
     }
 }
 
-// addCerts adds certificates into Notation trust store.
-function addCerts(dir: string) {
-    let trustStores = fs.readdirSync(dir, {withFileTypes: true, recursive: false})
+// configTrustStore configures Notation trust store based on specs.
+// Reference: https://github.com/notaryproject/notaryproject/blob/main/specs/trust-store-trust-policy.md#trust-store
+function configTrustStore(dir: string) {
+    let trustStoreX509 = path.resolve(dir, X509); // .github/truststore/x509
+    if (!fs.existsSync(trustStoreX509)) {
+        throw new Error("cannot find dir: <trust_store>/x509");
+    }
+    let trustStoreTypes = getSubdir(trustStoreX509); // [.github/truststore/x509/ca, .github/truststore/x509/signingAuthority, ...]
+    for (let i = 0; i < trustStoreTypes.length; ++i) {
+        let trustStores = getSubdir(trustStoreTypes[i]); // [.github/truststore/x509/ca/<my_store1>, .github/truststore/x509/ca/<my_store2>, ...]
+        for (let j = 0; j < trustStores.length; ++j) {
+            let trustStore = trustStores[j]; // .github/truststore/x509/ca/<my_store>
+            let trustStoreName = path.basename(trustStore); // <my_store>
+            let certFile = getFileFromDir(trustStore); // [.github/truststore/x509/ca/<my_store>/<my_cert1>, .github/truststore/x509/ca/<my_store>/<my_cert2>, ...]
+            execSync(`notation cert add -t ${trustStoreTypes[i]} -s ${trustStoreName} ${certFile.join(' ')}`, { encoding: 'utf-8' });
+        }
+    }
+}
+
+// getSubdir gets all sub dirs under dir without recursive
+function getSubdir(dir: string): string[] {
+    return fs.readdirSync(dir, {withFileTypes: true, recursive: false})
     .filter(item => item.isDirectory())
     .map(item => path.resolve(dir, item.name));
-    for (let i = 0; i < trustStores.length; ++i) {
-        let trustStorePath = trustStores[i];
-        let trustStoreName = path.basename(trustStorePath);
-        let certFilePaths = fs.readdirSync(trustStorePath, {withFileTypes: true, recursive: false})
-                    .filter(item => !item.isDirectory())
-                    .map(item => path.resolve(trustStorePath, item.name));
-        execSync(`notation cert add -t ca -s ${trustStoreName} ${certFilePaths.join(' ')}`, { encoding: 'utf-8' });
-    }
+}
+
+// getSubdir gets all files under dir without recursive
+function getFileFromDir(dir: string): string[] {
+    return fs.readdirSync(dir, {withFileTypes: true, recursive: false})
+    .filter(item => !item.isDirectory())
+    .map(item => path.resolve(dir, item.name));
 }
 
 export = verify;
