50add3726efeaf22fc859b0bcc86a61733429ea5
Patrick Zheng <patrickzheng@microsoft.com>
2023-08-09 12:33:33 +0800

    fix hash
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- src/sign.ts
+++ src/sign.ts
@@ -97,9 +97,8 @@ async function setupPlugin() {
 
 async function checkPluginExistence(pluginPath: string): Promise<boolean> {
     const pluginBinaryPath = path.join(pluginPath, `notation-${plugin_name}`);
-    console.log(`pluginBinaryPath is ${pluginBinaryPath}`);
     if (fs.existsSync(pluginBinaryPath)) {
-        const sha256 = await hash(pluginPath);
+        const sha256 = await hash(pluginBinaryPath);
         return sha256 === plugin_checksum;
     }
     return false
